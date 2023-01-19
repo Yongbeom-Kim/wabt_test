3f975d4bfea6e316eed4f1110be2c9200a7dd490
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-01-19 23:04:35 +0800

    trying top-level await

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import wabt from 'wabt';
+import { default as w } from 'wabt';
 
-wabt().then(wabt => {
+const wabt = await w();
 
-    const wat = `
+const wat = `
     (module
         (func $i (import "imports" "imported_func") (param i32))
         (func (export "exported_func")
@@ -12,20 +12,17 @@ wabt().then(wabt => {
         )
     `; // a buffer holding the contents of a wasm file
 
-    const importObject = {
-        imports: { imported_func: console.log },
-    };
+const importObject = {
+    imports: { imported_func: console.log },
+};
 
-    const myModule = wabt.parseWat("", wat);
+const myModule = wabt.parseWat("", wat);
 
-    const wasmModule = new WebAssembly.Module(myModule.toBinary({}).buffer);
+const wasmModule = new WebAssembly.Module(myModule.toBinary({}).buffer);
 
-    const wasmInstance = WebAssembly.instantiate(myModule.toBinary({}).buffer, importObject);
+const wasmInstance = WebAssembly.instantiate(myModule.toBinary({}).buffer, importObject);
 
-    wasmInstance.then(instance => {
-        instance.instance.exports.exported_func();
-    })
-    
-
+wasmInstance.then(instance => {
+    instance.instance.exports.exported_func();
 })
 
